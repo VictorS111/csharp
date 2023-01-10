32872ae7711aba1cfaed98486386b4b53f256df5
Viktor Sh <Viktor252007@gmail.com>
2023-01-10 12:17:01 +0200

    First version

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+// using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+
+
+namespace project;
+
+class Program 
+{ 
+
+    public static void Main(string[] args) 
+    {
+        
+    }  
+}
+
+
+
+
+
